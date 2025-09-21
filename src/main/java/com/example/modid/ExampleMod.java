74b59037c8bb7471dd24a77ad672a005f3481a32
JXSnack <jx.snack@gmail.com>
2025-09-21 21:41:39 +0200

    rename accordingly

--- src/main/java/com/example/modid/ExampleMod.java
+++ src/main/java/com/example/modid/ExampleMod.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ExampleMod implements ModInitializer {
-	public static final String MOD_ID = "modid";
+	public static final String MOD_ID = "exampleid";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
